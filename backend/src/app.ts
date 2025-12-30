0eb5a5f8dc7b51e1b9ceba483c1120d91b0fc853
westonz7042 <westonz7042@gmail.com>
2025-12-29 22:10:17 -0800

    initial backend setup

---
+++ backend/src/app.ts
@@ -0,0 +1,34 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+
+import { FRONTEND_ORIGIN, MONGO_URI, PORT } from "./config";
+import errorHandler from "./middleware/errorHandler";
+import log from "./middleware/logger";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: FRONTEND_ORIGIN,
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+  })
+);
+
+app.use(express.json());
+
+app.use(log);
+
+app.use(errorHandler);
+app.listen(PORT, () => {
+  console.info(`> Listening on port ${PORT}`);
+});
+// mongoose
+//   .connect(MONGO_URI)
+//   .then(() => {
+//     console.info("Mongoose connected!");
+//     app.listen(PORT, () => {
+//       console.info(`> Listening on port ${PORT}`);
+//     });
+//   })
+//   .catch(console.error);
---
+++ backend/src/config.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+
+import { InternalError } from "./errors/internal";
+
+dotenv.config();
+
+function throwIfUndefined(
+  envVar: string | undefined,
+  error: InternalError
+): string {
+  if (!envVar) throw error;
+  return envVar;
+}
+
+const PORT = throwIfUndefined(process.env.APP_PORT, InternalError.NO_APP_PORT);
+const MONGO_URI = throwIfUndefined(
+  process.env.MONGO_URI,
+  InternalError.NO_MONGO_URI
+);
+const FRONTEND_ORIGIN = throwIfUndefined(
+  process.env.FRONTEND_ORIGIN,
+  InternalError.NO_FRONTEND_ORIGIN
+);
+
+export { FRONTEND_ORIGIN, MONGO_URI, PORT };
