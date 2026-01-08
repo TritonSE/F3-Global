0c9b1cd90d29344515c8c3ad757a1e0b94aad213
westonz7042 <westonz7042@gmail.com>
2026-01-08 12:53:18 -0800

    mongo connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 import express from "express";
-
-import { FRONTEND_ORIGIN, PORT } from "./config";
+import mongoose from "mongoose";
+import { FRONTEND_ORIGIN, PORT, MONGO_URI } from "./config";
 import errorHandler from "./middleware/errorHandler";
 import log from "./middleware/logger";
 
@@ -19,15 +19,12 @@ app.use(express.json());
 app.use(log);
 
 app.use(errorHandler);
-app.listen(PORT, () => {
-  console.info(`> Listening on port ${PORT}`);
-});
-// mongoose
-//   .connect(MONGO_URI)
-//   .then(() => {
-//     console.info("Mongoose connected!");
-//     app.listen(PORT, () => {
-//       console.info(`> Listening on port ${PORT}`);
-//     });
-//   })
-//   .catch(console.error);
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.info("Mongoose connected!");
+    app.listen(PORT, () => {
+      console.info(`> Listening on port ${PORT}`);
+    });
+  })
+  .catch(console.error);
