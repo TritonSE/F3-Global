59c3ee21c5462aa54556deafe0a6882925449ac6
katelyn <katelynlitree@gmail.com>
2026-01-20 18:27:19 -0800

    added footer to layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 
+import { Footer } from "../components/Footer";
+
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -25,7 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
