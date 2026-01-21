1d8f9e3028e621980a020dcc679169f051fe7ddb
James Escobedo <james@escobe.do>
2026-01-20 18:15:48 -0800

    Add home page UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,65 +1,155 @@
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
+  const cities = [
+    "Hong Kong",
+    "Mumbai",
+    "São Paulo",
+    "Tokyo",
+    "San Diego",
+    "Mexico City",
+    "London",
+    "Seattle",
+    "Jakarta",
+    "Cairo",
+  ];
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <>
+      <div>
+        <div className="relative w-full min-h-screen bg-[#f6f6f6] overflow-x-hidden">
+          <div className="absolute top-0 left-0 w-full h-screen pointer-events-none select-none overflow-hidden">
+            <div className="absolute -top-[35%] -right-[55%] w-[120%] h-[120%] opacity-25">
+              <Image
+                src="/worldmap.png"
+                alt="World Map Background"
+                fill
+                className="object-cover"
+                priority
+                style={{
+                  objectPosition: "100% 0%",
+                }}
+              />
+            </div>
+
+            <div
+              className="absolute inset-0"
+              style={{
+                background:
+                  "linear-gradient(197.05deg, rgba(246, 246, 246, 0) 12.21%, rgb(246, 246, 246) 68.32%)",
+              }}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+
+          <div className="relative z-10 w-full pl-[5vw] pt-[8vw] m-left">
+            <h1 className="text-[#172447] text-[8vw] leading-[0.92] font-ethic font-light [font-feature-settings:'dlig'_on]">
+              <span className="block italic">Empowering</span>
+              <span className="block">Small</span>
+              <span className="block">Businesses</span>
+            </h1>
+            <p className="font-dm text-[#5D5D5D] text-[24px] leading-[32px] font-normal mt-8 max-w-3xl">
+              Join us in our mission to foster economic growth and financial inclusion for
+              underserved communities worldwide.
+            </p>
+            <div className="absolute left-0 right-0 flex justify-center items-center gap-[30px] mt-10">
+              <button className="flex justify-center items-center gap-[10px] px-[20px] py-[15px] bg-[#172447] rounded-[99px] text-white font-dm text-[16px] leading-[1.5] uppercase transition-colors duration-450 ease-in-out hover:bg-[#1169B0]">
+                become a client
+              </button>
+              <button className="flex justify-center items-center gap-[10px] px-[20px] py-[15px] bg-white border-[1.5px] border-[#C7C7C7] rounded-[99px] text-[#172447] font-dm text-[16px] leading-[1.5] uppercase transition-colors duration-450 ease-in-out hover:bg-[#C7C7C7] hover:border-black">
+                join as a member
+              </button>
+            </div>
+          </div>
+          <div className="w-full mt-38 overflow-hidden flex">
+            <div className="flex animate-marquee whitespace-nowrap">
+              {cities.map((city, index) => (
+                <div key={`city-1-${index}`} className="flex items-center">
+                  <span className="text-[#5D5D5D] text-[32px] leading-[32px] font-ethic font-light italic [font-feature-settings:'dlig'_on]">
+                    {city}
+                  </span>
+                  <span className="text-[#5D5D5D] text-[32px] leading-[32px] mx-6">•</span>
+                </div>
+              ))}
+
+              {cities.map((city, index) => (
+                <div key={`city-2-${index}`} className="flex items-center">
+                  <span className="text-[#5D5D5D] text-[32px] leading-[32px] font-ethic font-light italic [font-feature-settings:'dlig'_on]">
+                    {city}
+                  </span>
+                  <span className="text-[#5D5D5D] text-[32px] leading-[32px] mx-6">•</span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="relative z-20 w-full mt-20 px-[5vw] flex flex-col items-center">
+            <h2 className="text-[#172447] text-[96px] leading-[1.5] font-ethic font-light italic mb-12">
+              Mission
+            </h2>
+
+            <p className="text-[#1E1E1E] text-center font-dm text-[24px] font-normal leading-[32px] max-w-4xl self-stretch mx-auto">
+              Microfinancing is transforming lives across continents, empowering{" "}
+              <span className="text-[#012060] font-bold">millions</span> with access to essential
+              financial services. Join us in our mission to foster{" "}
+              <span className="text-[#012060] font-bold">economic growth</span> and financial
+              inclusion for{" "}
+              <span className="text-[#012060] font-bold">underserved communities</span> worldwide.
+            </p>
+          </div>
+          <div className="relative z-20 w-full mt-32 px-[5vw] flex gap-6 flex-col items-start pb-20">
+            <h3 className="text-[#172447] font-dm text-[48px] font-medium leading-[1.5] tracking-[-0.96px]">
+              Building Futures Together
+            </h3>
+            <div className="self-stretch flex justify-between items-start p-[25px] rounded-[10px] bg-[#EBEBEB]">
+              <div className="w-[375px] flex flex-col items-start self-stretch">
+                <h4 className="text-[#012060] font-ethic text-[64px] font-light leading-[1.1]">
+                  $10K
+                </h4>
+                <p className="text-[#1E1E1E] font-dm text-[24px] font-semibold leading-[1.5]">
+                  Total Raised
+                </p>
+                <p className="text-[#5D5D5D] font-dm text-[16px] font-normal leading-[1.5]">
+                  Deployed as microloans to entrepreneurs lacking traditional banking access,
+                  providing capital to launch businesses and lift families out of poverty.
+                </p>
+              </div>
+
+              <div className="w-[2px] h-48 bg-[#C7C7C7] mx-6 self-center"></div>
+
+              <div className="w-[375px] flex flex-col items-start self-stretch">
+                <h4 className="text-[#012060] font-ethic text-[64px] font-light leading-[1.1]">
+                  57
+                </h4>
+                <p className="text-[#1E1E1E] font-dm text-[24px] font-semibold leading-[1.5]">
+                  Members Worldwide
+                </p>
+                <p className="text-[#5D5D5D] font-dm text-[16px] font-normal leading-[1.5]">
+                  Funding microloans that help entrepreneurs across continents launch businesses,
+                  create jobs, and build sustainable futures for their communities.
+                </p>
+              </div>
+
+              <div className="w-[2px] h-48 bg-[#C7C7C7] mx-6 self-center"></div>
+
+              <div className="w-[375px] flex flex-col items-start self-stretch">
+                <h4 className="text-[#012060] font-ethic text-[64px] font-light leading-[1.1]">
+                  25
+                </h4>
+                <p className="text-[#1E1E1E] font-dm text-[24px] font-semibold leading-[1.5]">
+                  Organizations Supported
+                </p>
+                <p className="text-[#5D5D5D] font-dm text-[16px] font-normal leading-[1.5]">
+                  Invested directly into entrepreneurs worldwide, providing the capital they need to
+                  launch businesses, create employment, and build thriving futures.
+                </p>
+              </div>
+            </div>
+            <p className="text-[#5D5D5D] font-dm text-[16px] font-bold leading-[1.5] mt-4 self-start uppercase">
+              *Data from Jan 2026
+            </p>
+          </div>
         </div>
-      </main>
-    </div>
+      </div>
+    </>
   );
 }
