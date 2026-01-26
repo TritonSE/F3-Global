14ed92f8d5a51d5f3e582e5b268225753ecdd00d
James Escobedo <james@escobe.do>
2026-01-26 11:35:14 -0800

    Fix button routing

--- frontend/src/app/meet-the-team/page.tsx
+++ frontend/src/app/meet-the-team/page.tsx
@@ -34,7 +34,7 @@ export default function MeetTheTeam() {
             <div>
               <Button
                 text="join our team"
-                onClick_link="/members"
+                onClick_link="https://my-apply.vercel.app/org/f3-global-foundation"
                 className="flex justify-center items-center gap-[10px] px-[24px] py-[14px] bg-[#172447] rounded-[99px] transition-colors duration-450 ease-in-out hover:bg-[#1169B0] [&_p]:uppercase [&_p]:font-normal mt-12"
               />
             </div>
