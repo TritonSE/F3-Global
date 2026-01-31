7cd12199c8543c69192557069e3609ba2dd19442
Yasmin Kabir <yasminkabirofficial@gmail.com>
2026-01-30 20:46:19 -0800

    probably done now?

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState, useRef } from "react";
+import { useRef, useState } from "react";
 
 function NavLinks({
   isActive,
