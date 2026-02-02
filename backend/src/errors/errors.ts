6740e80f00d1a4bc13ca562c38249367d0c49532
westonz7042 <westonz7042@gmail.com>
2026-02-01 23:41:25 -0800

    client and server firebase setup

--- backend/src/errors/errors.ts
+++ backend/src/errors/errors.ts
@@ -2,6 +2,7 @@ export class CustomError extends Error {
   public code: number;
   public status: number;
   public message: string;
+  public context: string[];
 
   /**
    * @param code      Error code
@@ -13,5 +14,30 @@ export class CustomError extends Error {
     this.code = code;
     this.status = status;
     this.message = message;
+    this.context = [];
+  }
+  /**
+   * This function allows you to add context to error messages
+   * such as stack traces
+   * @param message Message to be added
+   */
+  public addContext(message: string) {
+    this.context.push(message);
+    return this.context;
+  }
+
+  /**
+   * This method will display the error message
+   * based on whether its client facing or not
+   * @param clientFacing: determines whether the message is client facing
+   */
+  public displayMessage(clientFacing: boolean) {
+    if (clientFacing) {
+      return this.message;
+    }
+
+    return `Error: Type ${this.constructor.name}, Code ${this.code.toString()}, Context: ${
+      this.context.length ? `\n${this.context.join("\n\n")}` : ""
+    }`;
   }
 }
