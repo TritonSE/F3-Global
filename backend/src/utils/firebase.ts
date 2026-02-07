83715473488f7dbd0d64e3d31ffc484e86e1a56e
katelyn <katelynlitree@gmail.com>
2026-02-07 10:57:26 -0800

    firebase bucket config

--- backend/src/utils/firebase.ts
+++ backend/src/utils/firebase.ts
@@ -1,13 +1,19 @@
 import * as firebaseAdmin from "firebase-admin/app";
 import { getAuth as getAdminAuth } from "firebase-admin/auth";
+import "dotenv/config";
 
 import { SERVICE_ACCOUNT_KEY } from "../config";
 
 import type { ServiceAccount } from "firebase-admin/app";
 
+const storageBucket = process.env.STORAGE_BUCKET;
+if (!storageBucket) {
+  throw new Error("STORAGE_BUCKET is not defined in environment variables");
+}
+
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.cert(JSON.parse(SERVICE_ACCOUNT_KEY) as ServiceAccount),
-  storageBucket: "f3-global.firebasestorage.app",
+  storageBucket,
 });
 
 const firebaseAdminAuth = getAdminAuth();
