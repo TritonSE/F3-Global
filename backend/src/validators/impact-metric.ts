a3bbeec922afbb122380aaa6b9729a837c3638a1
James Escobedo <james@escobe.do>
2026-02-14 13:44:00 -0800

    Add length validation comment

--- backend/src/validators/impact-metric.ts
+++ backend/src/validators/impact-metric.ts
@@ -11,13 +11,18 @@ export const updateImpactMetric = [
     .isInt({ min: 0, max: 2 })
     .withMessage("order must be 0, 1, or 2"),
 
+  /* the length validator for eachwas created/tested in kind of a dumb way, i tested a wide character like `W` and
+   * saw how many it took to overflow the container, then added a couple chars for leeway. wondering if theres
+   * maybe a smarter way to validate length?
+   */
+
   body("metrics.*.statistic")
     .optional()
     .isString()
     .withMessage("statistic must be a string")
     .notEmpty()
     .withMessage("statistic cannot be empty")
-    .isLength({ max: 6 }) // any more overflows on standard 16:10 resolution, same w/ rest of length constraints
+    .isLength({ max: 8 })
     .withMessage("statistic max length is 6"),
 
   body("metrics.*.subtitle")
