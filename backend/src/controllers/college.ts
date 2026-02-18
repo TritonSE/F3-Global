01644737ba0bfaf3e5344d1b68b39932a0ec501c
Yasmin Kabir <yasminkabirofficial@gmail.com>
2026-02-18 12:19:39 -0800

    push backend changes

---
+++ backend/src/controllers/college.ts
@@ -0,0 +1,15 @@
+// import {Request, Response, NextFunction } from "express";
+// import { validationResult } from "express-validator";
+// import College from "../models/college";
+// import {deleteImageFromFirebase} from "../utils/firebaseStorage";
+// import validationErrorParser from "../utils/validationErrorParser";
+
+// export const getAllColleges = async (
+//     req: Request,
+//     res: Response,
+//     next: NextFunction
+// ) => {
+//     try {
+
+//     }
+// }
--- backend/src/validators/college.ts
+++ backend/src/validators/college.ts
@@ -1,1 +1,15 @@
-// import { body } from "express-validator";
+import { body } from "express-validator";
+
+export const createCollegeValidator = [
+  body("name")
+    .exists({ checkFalsy: true })
+    .withMessage("Name is required")
+    .isString()
+    .withMessage("Name must be a string"),
+
+  body("imageUrl")
+    .exists({ checkFalsy: true })
+    .withMessage("Image URL is required")
+    .isString()
+    .withMessage("Image URL must be a string"),
+];
