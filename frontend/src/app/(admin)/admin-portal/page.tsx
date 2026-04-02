60a6a86072b0097c75773f239c7507aa9929b7af
katelyn <katelynlitree@gmail.com>
2026-04-02 01:19:09 -0700

    finished page

--- frontend/src/app/(admin)/admin-portal/page.tsx
+++ frontend/src/app/(admin)/admin-portal/page.tsx
@@ -1,16 +1,140 @@
-import { ScrollTopButton } from "@/components/ScrollTopButton";
+import { Section } from "@/components/admin-portal/Section";
 
 export default function AdminPortal() {
+  const HomeComponents = [
+    {
+      name: "Rotating Cities",
+      description:
+        "Edit city names by clicking into the text box, reorder by dragging, or add/delete cities.",
+      tags: ["Homepage"],
+      href: "/rotating-cities-editor",
+    },
+    {
+      name: "Impact Metrics",
+      description:
+        "Input total money raised, how many members F3 has worldwide, and how many organizations have been supported, along with the date last updated.",
+      tags: ["Homepage", "Donors"],
+      href: "/impact-metrics-editor",
+    },
+    {
+      name: "Client Highlights",
+      description: "Edit 3 different client stories and their pictures. One story must be primary.",
+      tags: ["Homepage", "About Us", "Donors"],
+      href: "/client-highlights-editor",
+    },
+  ];
+
+  const AboutComponents = [
+    {
+      name: "Clients Logos",
+      description: "Replace client logos, reorder by dragging, or add/remove logos.",
+      tags: ["About Us"],
+      href: "/client-logos-editor",
+    },
+    {
+      name: "Client Highlights",
+      description: "Edit 3 different client stories and their pictures. One story must be primary.",
+      tags: ["Homepage", "About Us", "Donors"],
+      href: "/client-highlights-editor",
+    },
+    {
+      name: "Affiliation Logos",
+      description:
+        "Edit, add, or remove a testimonial from the Clients Carousel which rotates between a set of three client testimonials.",
+      tags: ["Homepage", "About Us", "Donors"],
+      href: "/affiliation-logos-editor",
+    },
+  ];
+
+  const TeamComponents = [
+    {
+      name: "College Logos",
+      description: "Replace college logos, reorder by dragging, or add/remove logos.",
+      tags: ["Meet the Team"],
+      href: "/college-logos-editor",
+    },
+    {
+      name: "Edit Team Members",
+      description:
+        "Add or remove current F3 team members, or edit information including member locations, image, and contact links.",
+      tags: ["Meet the Team"],
+      href: "/team-members-editor",
+    },
+  ];
+
+  const DonorComponents = [
+    {
+      name: "Client Highlights",
+      description: "Edit 3 different client stories and their pictures. One story must be primary.",
+      tags: ["Homepage", "About Us", "Donors"],
+      href: "/client-highlights-editor",
+    },
+    {
+      name: "Impact Metrics",
+      description:
+        "Input total money raised, how many members F3 has worldwide, and how many organizations have been supported, along with the date last updated.",
+      tags: ["Homepage", "Donors"],
+      href: "/impact-metrics-editor",
+    },
+    {
+      name: "Frequently Asked Questions",
+      description:
+        "Reorder FAQs by dragging, or easily add, edit, or remove questions and answers.",
+      tags: ["Donors", "Members", "Clients"],
+      href: "/faqs-editor",
+    },
+  ];
+
+  const MemberComponents = [
+    {
+      name: "Frequently Asked Questions",
+      description:
+        "Reorder FAQs by dragging, or easily add, edit, or remove questions and answers.",
+      tags: ["Donors", "Members", "Clients"],
+      href: "/faqs-editor",
+    },
+  ];
+
+  const ClientComponents = [
+    {
+      name: "Client Highlights",
+      description: "Edit 3 different client stories and their pictures. One story must be primary.",
+      tags: ["Homepage", "About Us", "Donors"],
+      href: "/client-highlights-editor",
+    },
+    {
+      name: "Frequently Asked Questions",
+      description:
+        "Reorder FAQs by dragging, or easily add, edit, or remove questions and answers.",
+      tags: ["Donors", "Members", "Clients"],
+      href: "/faqs-editor",
+    },
+  ];
+
+  const NewsletterComponents = [
+    {
+      name: "Edit Newsletter Articles",
+      description:
+        "Add or remove newsletter articles, or edit information including the newsletter external PDF link and summary. Track how many views each article has. By default, articles appear sorted by date of upload, and the Featured article is always sticky to the top.",
+      tags: ["Newsletter"],
+      href: "/newsletter-articles-editor",
+    },
+  ];
+
   return (
-    <div className="flex p-[50px] gap-[50px] flex-col items-start">
-      <p className="text-[#1E1E1E] text-[32px] font-bold -tracking-[0.64px]">Welcome, F3 GLOBAL</p>
-      <div className="flex w-full gap-[25px]">
-        <div className="flex w-full pb-[5px] border-b-[1px] border-[#172447] justify-between">
-          <p className="text-[#172447] text-[24px] font-bold tracking-[3px] ">HOME</p>
-          <ScrollTopButton />
-        </div>
-        {/* div for the components section */}
+    <div className="flex p-[50px] gap-[40px] flex-col items-start">
+      <div className="flex gap-[10px] -mb-[10px]">
+        <p className="text-[#1E1E1E] text-[32px] font-bold -tracking-[0.64px]">Welcome,</p>
+        <p className="text-[#172447] text-[32px] font-bold -tracking-[0.64px]">F3 Global!</p>
       </div>
+
+      <Section name={"HOME"} components={HomeComponents} />
+      <Section name={"ABOUT US"} components={AboutComponents} />
+      <Section name={"MEET THE TEAM"} components={TeamComponents} />
+      <Section name={"DONATE"} components={DonorComponents} />
+      <Section name={"MEMBERS"} components={MemberComponents} />
+      <Section name={"CLIENTS"} components={ClientComponents} />
+      <Section name={"NEWSLETTER"} components={NewsletterComponents} />
     </div>
   );
 }
---
+++ frontend/src/components/admin-portal/ComponentSection.tsx
@@ -0,0 +1,49 @@
+"use client";
+import Image from "next/image";
+
+type ComponentSectionProps = {
+  name: string;
+  description: string;
+  tags: string[];
+  href: string;
+};
+
+export const ComponentSection = function ComponentSection({
+  name,
+  description,
+  tags,
+  href,
+}: ComponentSectionProps) {
+  return (
+    <div className="flex p-[10px] justify-center items-center gap-[10px] rounded-2xl border-[1px] border-[#F4F4F4]">
+      <div
+        className="flex flex-col w-[290px] rounded-2xl border-[1px] border-[#D9D9D9] cursor-pointer"
+        onClick={() => (window.location.href = href)}
+      >
+        <Image
+          src="/imgs/components_placeholder.png"
+          alt="component img"
+          width={290}
+          height={183}
+          className="rounded-t-2xl"
+        />
+        <div className="flex flex-col px-[16px] py-[10px] items-start gap-[10px]">
+          <div className="flex flex-col gap-[4px]">
+            <p className="text-[#000] text-[16px] font-semibold ">{name}</p>
+            <p className="text-[#6C6C6C] text-[12px] font-normal leading-[16px]">{description}</p>
+          </div>
+          <div className="flex items-center gap-[5px]">
+            {tags.map((tag) => (
+              <div
+                key={tag}
+                className="flex px-[8px] py-[2px] justify-center items-center rounded-[99px] bg-[#A5D0F2]"
+              >
+                <p className="text-[#172447] text-[10px]">{tag}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
