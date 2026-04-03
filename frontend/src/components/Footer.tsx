d023cf6a1cc2e1117b7c448d349cd93c1a8b57c6
katelyn li <103160875+katelnyli@users.noreply.github.com>
2026-04-02 23:56:40 -0700

    Remove unused usePathname import from Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 import { FooterMiniBtn } from "./FooterMiniBtn";
 
