83f0b34a44c15de42ccb3634f77228c716aeb1d0
jadenhuang88@gmail.com <jadenhuang88@gmail.com>
2026-04-19 15:26:28 -0700

    protected

--- backend/src/routes/colleges.ts
+++ backend/src/routes/colleges.ts
@@ -1,14 +1,30 @@
 import express from "express";
 
 import * as CollegeController from "../controllers/college";
+import { verifyAuthToken } from "../validators/auth";
 import * as CollegeValidator from "../validators/college";
 
 const router = express.Router();
 
 router.get("/all", CollegeController.getAllColleges);
 router.get("/", CollegeController.getAllColleges);
-router.post("/", CollegeValidator.createCollegeValidator, CollegeController.createCollege);
-router.put("/", CollegeValidator.updateCollegesValidator, CollegeController.updateColleges);
-router.delete("/:id", CollegeValidator.deleteCollegeValidator, CollegeController.deleteCollege);
+router.post(
+  "/",
+  verifyAuthToken,
+  CollegeValidator.createCollegeValidator,
+  CollegeController.createCollege,
+);
+router.put(
+  "/",
+  verifyAuthToken,
+  CollegeValidator.updateCollegesValidator,
+  CollegeController.updateColleges,
+);
+router.delete(
+  "/:id",
+  verifyAuthToken,
+  CollegeValidator.deleteCollegeValidator,
+  CollegeController.deleteCollege,
+);
 
 export default router;
